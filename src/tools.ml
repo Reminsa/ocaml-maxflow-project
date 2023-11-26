0249aa7788fba5a88064d66a60c85fc7fdaa7d54
Remi-Jacquemin <remibenja@hotmail.fr>
2023-11-26 22:01:34 +0100

    potential solution gmap

--- src/tools.ml
+++ src/tools.ml
@@ -9,6 +9,10 @@ let clone_nodes gr = n_fold gr (fun g id -> new_node g id) empty_graph;;
 (*let gmap gr f =   e_iter gr (fun x -> f x ) ;;*)
 
 (*let gmap gr f =  e_fold gr (fun g arc -> e_iter gr (fun x -> f x)) empty_graph ;;*)
+
+(*let gmap gr f =
+  let map_arc arc = { arc with lbl = f arc.lbl } in
+  e_fold gr (fun acc arc -> new_arc acc (map_arc arc)) empty_graph *)
   
 let gmap gr f  =
   e_fold gr (fun acc arc -> new_arc acc { arc with lbl = f arc.lbl }) (clone_nodes gr);;
