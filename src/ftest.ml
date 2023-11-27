843dd7d77e81ba3e461afa995a1e4a3b7f3041ec
Remi Jacquemin <rjacquem@insa-toulouse.fr>
2023-11-27 09:40:22 +0100

    add_arc

--- src/ftest.ml
+++ src/ftest.ml
@@ -3,43 +3,42 @@ open Tools
     
 let () =
 
-  (* Check the number of command-line arguments *)
-  if Array.length Sys.argv <> 5 then
-    begin
-      Printf.printf
-        "\n ✻  Usage: %s infile source sink outfile\n\n%s%!" Sys.argv.(0)
-        ("    🟄  infile  : input file containing a graph\n" ^
-         "    🟄  source  : identifier of the source vertex (used by the ford-fulkerson algorithm)\n" ^
-         "    🟄  sink    : identifier of the sink vertex (ditto)\n" ^
-         "    🟄  outfile : output file in which the result should be written.\n\n") ;
-      exit 0
-    end ;
+	(* Check the number of command-line arguments *)
+  	if Array.length Sys.argv <> 5 then
+    	begin
+			Printf.printf
+				"\n ✻  Usage: %s infile source sink outfile\n\n%s%!" Sys.argv.(0)
+				("    🟄  infile  : input file containing a graph\n" ^
+				"    🟄  source  : identifier of the source vertex (used by the ford-fulkerson algorithm)\n" ^
+				"    🟄  sink    : identifier of the sink vertex (ditto)\n" ^
+				"    🟄  outfile : output file in which the result should be written.\n\n") ;
+			exit 0
+    	end ;
 
 
-  (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
+  	(* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
   
-  let infile = Sys.argv.(1)
-  and outfile = Sys.argv.(4)
+  	let infile = Sys.argv.(1)
+  	and outfile = Sys.argv.(4)
   
   (* These command-line arguments are not used for the moment. *)
-  and _source = int_of_string Sys.argv.(2)
-  and _sink = int_of_string Sys.argv.(3)
-  in
+	and _source = int_of_string Sys.argv.(2)
+  	and _sink = int_of_string Sys.argv.(3)
+ 	in
 
-  (* Open file *)
-   let graph = from_file infile in
+ 	(* Open file *)
+  	let graph = from_file infile in
 
-  (* let graph = clone_nodes graph in*)
+ 	(* let graph = clone_nodes graph in*)
 
- (*let graph = gmap graph (fun arc -> arc.src = arc.src + 100) in*)
+	let graph = gmap graph (fun x -> int_of_string x) in 
 
- let graph2 = gmap graph (fun _ -> "yolo") in 
+	let graph = add_arc graph 0 5 100 in
 
+	let graph = gmap graph (fun x -> string_of_int x) in
 
+  	(* Rewrite the graph that has been read. *)
+  	let () = write_file outfile graph in
 
-
-  (* Rewrite the graph that has been read. *)
-  let () = write_file outfile graph2 in
-
-  ()
+  	()
 
--- src/tools.ml
+++ src/tools.ml
@@ -1,20 +1,19 @@
 open Graph
 
-(* assert false is of type ∀α.α, so the type-checker is happy. *)
-
-
-
 let clone_nodes gr = n_fold gr (fun g id -> new_node g id) empty_graph;;
-
-(*let gmap gr f =   e_iter gr (fun x -> f x ) ;;*)
-
-(*let gmap gr f =  e_fold gr (fun g arc -> e_iter gr (fun x -> f x)) empty_graph ;;*)
-
-(*let gmap gr f =
-  let map_arc arc = { arc with lbl = f arc.lbl } in
-  e_fold gr (fun acc arc -> new_arc acc (map_arc arc)) empty_graph *)
   
 let gmap gr f  =
   e_fold gr (fun acc arc -> new_arc acc { arc with lbl = f arc.lbl }) (clone_nodes gr);;
 
-let add_arc _graph _id1 _id2 _x = assert false  
+  let add_arc g id1 id2 n =
+    let arc = { src = id1; tgt = id2; lbl = n } in
+    try
+      let existing_arc = match find_arc g id1 id2 with
+        | Some a -> a
+        | None -> raise Not_found
+      in
+      let updated_arc = { existing_arc with lbl = existing_arc.lbl + n } in
+      new_arc g updated_arc
+    with
+    | Not_found -> new_arc g arc
+  
